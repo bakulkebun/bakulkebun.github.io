e3fa1842d3ecd2199d53247eb08aaac8325530d3
webmurahku <webmurahku@outlook.com>
2021-05-05 10:35:59 +0700

    make gsap local file

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 const contactButton = document.getElementById("contactButtonId");
 const contactGreeting = "Hallo,%20saya%20mau%20bertanya%20mengenai%20produk%20dari%20Bakul%20Kebun";
 const contactNumber = "628113023262";
